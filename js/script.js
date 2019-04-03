863144774bcf923d5734474d2f7c13c329fca5c7
fabianzielinski <fabz@o2.pl>
2019-04-03 22:39:35 +0200

    Changes after comments, again.

--- js/script.js
+++ js/script.js
@@ -12,21 +12,34 @@ var conFactor = 1.8; // conversion factor //
 outputMsg.innerHTML = ' "Click the button! I acout degrees..." ' + '<br><br>' + outputMsg.innerHTML; 
 
 buttonCes = document.getElementById('btnCes');
+
 buttonFht = document.getElementById('btnFht');
 
 btnCes.addEventListener('click', function() {
 
 	var tempCes = window.prompt('What is the temperature in degrees Celsius?');	
 
-	var tempFht = ((tempCes*conFactor)+mpWater);
+	var tempFht = ((tempCes * conFactor) + mpWater);
 
-		if (isNaN(tempCes)) {outputMsg.innerHTML = " It' not a temperature !!! "; } 
+		if (isNaN(tempCes)) {
 
-		else if (tempCes > 15) {outputMsg.innerHTML = '" It is very nice ..."'; }
+			outputMsg.innerHTML = " It' not a temperature !!! "; 
+		} 
+
+		else if (tempCes > 15) {
+
+			outputMsg.innerHTML = '" It is very nice ..."'; 
+		}
 			 
-	    else if (tempCes <= 5) {outputMsg.innerHTML = '" Without a hat and scarf you will not get around "'; }
+	    else if (tempCes <= 5) {
+
+	    	outputMsg.innerHTML = '" Without a hat and scarf you will not get around "'; 
+	    }
 	
-	    else {outputMsg.innerHTML = '" Dress up warmer "'; }
+	    else {
+
+	    	outputMsg.innerHTML = '" Dress up warmer "'; 
+	    }
 	
    		outputTmp.innerHTML = '" Temperature is '+ Math.round(tempFht) +'  F "';	 
 })
@@ -35,15 +48,27 @@ btnFht.addEventListener('click', function() {
 
 	var tempFht = window.prompt('What is the temperature in degrees Farenheit?');	
 
-	var	tempCes = ((tempFht-mpWater)/conFactor)
+	var	tempCes = ((tempFht - mpWater) / conFactor);
 
-		if (isNaN(tempCes)) {outputMsg.innerHTML = " It' not a temperature !!! "; } 
+		if (isNaN(tempCes)) {
 
-		else if (tempCes > 15) {outputMsg.innerHTML = '" It is very nice ..."'; }
+			outputMsg.innerHTML = " It' not a temperature !!! "; 
+		} 
+
+		else if (tempCes > 15) {
+
+			outputMsg.innerHTML = '" It is very nice ..."'; 
+		}
 			 
-	    else if (tempCes <= 5) {outputMsg.innerHTML = '" Without a hat and scarf you will not get around "'; }
+	    else if (tempCes <= 5) {
+
+	    	outputMsg.innerHTML = '" Without a hat and scarf you will not get around "'; 
+	    }
 	
-	    else {outputMsg.innerHTML = '" Dress up warmer "'; }
+	    else {
+
+	    	outputMsg.innerHTML = '" Dress up warmer "'; 
+	    }
 	  	
 		outputTmp.innerHTML = '" Temperature is '+ Math.round(tempCes) +'  C "';
 })
